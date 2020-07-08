e4ee332c40783a6d9768963a96c925d7d811ec05
AndroidOnTheFly <sergiotutorshd@gmail.com>
2020-07-08 20:36:00 -0300

    Corrections'

--- system/src/main/java/com/ajs/deposity/view/controllers/clients/NewClientController.java
+++ system/src/main/java/com/ajs/deposity/view/controllers/clients/NewClientController.java
@@ -1,5 +1,12 @@
 package com.ajs.deposity.view.controllers.clients;
 
-public class NewClientController {
+import com.ajs.deposity.view.controllers.home.Menu;
+import com.jfoenix.controls.JFXDatePicker;
+import javafx.fxml.FXML;
+
+public class NewClientController extends Menu {
+
+    @FXML
+    private JFXDatePicker datePickerClient;
 
 }
